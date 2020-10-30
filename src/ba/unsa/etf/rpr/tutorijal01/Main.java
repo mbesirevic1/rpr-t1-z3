11e783df7d3c2d94968d28f1d9aa1bc81d71fb80
mbesirevic1 <mbesirevic1@etf.unsa.ba>
2020-10-30 17:00:53 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/tutorijal01/Main.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.tutorijal01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sat s = new Sat(16, 51, 22);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.pomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+    }
+}
